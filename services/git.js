1d5507c9871ab7b7f15ab708461b9928ef7b706b
Dmitry <zadima@gmail.com>
2019-09-15 16:51:59 +0300

    dry in git

--- services/git.js
+++ services/git.js
@@ -1,64 +1,62 @@
 const { spawn, exec } = require('child_process')
 
-// *TODO: add DRY if there's time | Created at: 14.Sep.2019
-exports.log = (path, commit = '') => new Promise((resolve, reject) => {
+function git(command, { path, acc = '' }, readFn) {
+  return new Promise((resolve, reject) => {
+    let errors = ''
+
+    const child = spawn(`git ${command}`, { cwd: path, shell: true })
+
+    child.stdout.on('data', data => {
+      acc = readFn(data, acc)
+    })
+    child.stderr.on('data', data => errors += data)
+
+    child.on('close', () => errors === '' ? resolve(acc) : reject(errors))
+    child.on('error', reject)
+  })
+}
+
+exports.log = (path, commit = '') => {
   const SEPARATOR = ';separator;'
-  const commits = []
-  let errors = ''
 
-  const child = spawn(`git log --pretty=format:"%H${SEPARATOR}%ad${SEPARATOR}%s" ${commit}`, {
-    cwd: path,
-    shell: true
-  })
+  return git(`log --pretty=format:"%H${SEPARATOR}%ad${SEPARATOR}%s" ${commit}`, { path, acc: [] },
+    (data, acc) => {
+      const commitEntities = data
+        .toString().split('\n').filter(Boolean)
+        .map(commit => {
+          const [hash, data, author] = commit.split(SEPARATOR)
+          return { hash, data, author }
+        })
 
-  child.stdout.on('data', data => {
-    const commitEntities = data
-      .toString().split('\n').filter(Boolean)
-      .map(commit => {
-        const [hash, data, author] = commit.split(SEPARATOR)
-        return { hash, data, author }
-      })
+      acc.push(...commitEntities)
+      return acc
+    })
+}
 
-    commits.push(...commitEntities)
-  })
+exports.diff = (path, commit = '') => git(`diff ${commit}`,
+  { path, acc: '' },
+  (data, acc) => acc += data
+)
 
-  child.stderr.on('data', data => errors += data)
+exports.tree = (path, commitHash = '') => git(`ls-tree --name-only -r ${commitHash}`,
+  { path, acc: [] },
+  (data, acc) => {
+    acc.push(...data.toString().split('\n'))
+    return acc
+  }
+)
 
-  child.on('close', () => errors === '' ? resolve(commits) : reject(errors))
-  child.on('error', err => reject(err))
-})
+exports.showFile = async (path, commitHash, filePath) => {
+  const chunks = await git(`--no-pager  show ${commitHash}:${filePath}`,
+    { path, acc: [] },
+    (data, acc) => {
+      acc.push(Buffer.from(data))
+      return acc
+    }
+  )
 
-exports.diff = (path, commit = '') => new Promise((resolve, reject) => {
-  let diff = ''
-  let errors = ''
-
-  const child = spawn(`git diff ${commit}`, {
-    cwd: path,
-    shell: true
-  })
-
-  child.stdout.on('data', data => diff += data)
-  child.stderr.on('data', data => errors += data)
-
-  child.on('close', () => errors === '' ? resolve(diff) : reject(errors))
-  child.on('error', err => reject(err))
-})
-
-exports.tree = async (repoPath, commitHash = '') => new Promise((resolve, reject) => {
-  const files = []
-  let errors = ''
-
-  const child = spawn(`git ls-tree --name-only -r ${commitHash}`, {
-    cwd: repoPath,
-    shell: true
-  })
-
-  child.stdout.on('data', data => files.push(...data.toString().split('\n')))
-  child.stderr.on('data', data => errors += data)
-
-  child.on('close', () => errors === '' ? resolve(files) : reject(errors))
-  child.on('error', err => reject(err))
-})
+  return Buffer.concat(chunks)
+}
 
 exports.getMainBranchHash = path => new Promise((resolve, reject) => {
   exec('git symbolic-ref --short HEAD | xargs git rev-parse', { cwd: path }, (err, stdout, stderr) => {
@@ -78,43 +76,16 @@ exports.getRevHash = (path, commitHash) => new Promise((resolve, reject) => {
   })
 })
 
-exports.showFile = (path, commitHash, filePath) => new Promise((resolve, reject) => {
-  const chunks = []
-  let errors = ''
-
-  const child = spawn(`git --no-pager show ${commitHash}:${filePath} `, {
-    cwd: path,
-    shell: true
-  })
-
-  child.stdout.on('data', data => chunks.push(Buffer.from(data)))
-  child.stderr.on('data', data => errors += data)
-
-  child.on('close', () => errors === ''
-    ? resolve(Buffer.concat(chunks))
-    : reject(errors)
-  )
-  child.on('error', err => reject(err))
-})
-
 exports.clone = (repoUrl, path) => new Promise((resolve, reject) => {
   let errors = ''
 
-  const child = spawn(`git clone ${repoUrl}`, {
-    cwd: path,
-    shell: true
-  })
+  const child = spawn(`git clone ${repoUrl}`, { cwd: path, shell: true })
 
   child.stderr.on('data', data => {
     // horrible crutch cause git clone writes to stderr
     if (!data.includes('Cloning into')) errors += data
   })
 
-  child.on('close', () => errors === ''
-    ? resolve()
-    : reject(errors)
-  )
-  child.on('error', err => {
-    reject(err)
-  })
+  child.on('close', () => errors === '' ? resolve() : reject(errors))
+  child.on('error', reject)
 })
