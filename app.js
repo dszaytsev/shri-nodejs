5a4c662c4e40557ffd14e36627e003f638119272
Dmitry <zadima@gmail.com>
2019-09-27 20:25:22 +0300

    render views with server

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Bundler = require('parcel-bundler')
 const path = require('path')
 const bodyParser = require('body-parser')
 const initDb = require('./initializers/initDb')
@@ -9,6 +10,14 @@ const pathToRepos = path.resolve(pathArg)
 
 const app = express()
 
+const bundler = new Bundler('client/index.js', {
+  outDir: 'public/assets'
+})
+app.use(bundler.middleware())
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
 app.use((req, res, next) => {
   res.reposPath = pathToRepos
   next()
@@ -16,6 +25,8 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes'))
 app.use((_, res) => res.send(404))
